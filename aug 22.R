0654d39cd07f7dd925cadcff2c1147afd1d95c2e
quandariuspat <qpatrick@claflin.edu>
2024-09-16 11:24:21 -0400

    Add files via upload

---
+++ aug 22.R
@@ -0,0 +1,23 @@
+#Author: Quandarius Patrick , Date: 08/22/24, Purpose: Test the mesaures of descriptive stats
+
+
+aug 29.txt
+
+# How to calculate standard deviation in R
+
+X <- c (1,2,3,4,5)
+
+I correlation.R
+
+sd(X)
+
+# How to calculate mean in Ri
+
+mean (X)
+
+# How?
+
+to calculate median in R
+
+median (X)
+
